8ca339befcc53f543dd9494b9989bf3c47a5a25d
rronier <ronier2332@gmail.com>
2021-01-08 04:02:27 +0100

    test pass, increse the count by one when the user clicks the button Add

--- src/FizzCounter/FizzCounter.js
+++ src/FizzCounter/FizzCounter.js
@@ -4,8 +4,19 @@ export default function FizzCounter ()
 {
     const [count, setCount] = useState(1)
 
+    const addCounter = () =>
+    {
+        setCount(count + 1)
+    }
+
     return (
         <div>
+            <div>
+                <button
+                    data-testid="addButton"
+                    disabled={count === 100}
+                    onClick={addCounter}>Add</button>
+            </div>
             <div>
                 <p data-testid="count">{FizzBuzz(count)}</p>
             </div>
