f018cb8fa5295b3ccc197b86c69df7d96be01684
rronier <ronier2332@gmail.com>
2021-01-08 01:37:40 +0100

    Creating fizzbuzz react project base

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>FizzBuzz example upgraded</h1>
+    </div>
+  )
+}
+
+export default App
