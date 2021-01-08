05183e32a0423ad3da1c39b5aec44b9bce348fa0
rronier <ronier2332@gmail.com>
2021-01-08 04:00:30 +0100

    test fail, increse the count by one when the user clicks the button Add

--- src/FizzCounter/FizzCounter.test.js
+++ src/FizzCounter/FizzCounter.test.js
@@ -7,3 +7,10 @@ it('should display the start count at 1', () =>
     const {getByTestId} = render(<FizzCounter />)
     expect(getByTestId('count')).toHaveTextContent('1')
 })
+
+it('should increse the count by one when the user clicks the button Add', () =>
+{
+    const {getByTestId, getByText} = render(<FizzCounter />)
+    fireEvent.click(getByText("Add"))
+    expect(getByTestId("count")).toHaveTextContent("2")
+})
